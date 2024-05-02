f8aa95cb86b8aa8862d20081b1793a59846b148b
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-05-02 19:20:45 -0300

    Programa feito

---
+++ com.allogica.ControleFluxo/src/Contador.java
@@ -0,0 +1,31 @@
+import com.allogica.ControleFluxo.Exceptions.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = Integer.parseInt(terminal.nextLine());
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = Integer.parseInt(terminal.nextLine());
+
+        try {
+            contar(parametroUm, parametroDois);
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo número " + i);
+        }
+    }
+}
---
+++ com.allogica.ControleFluxo/src/com/allogica/ControleFluxo/Exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package com.allogica.ControleFluxo.Exceptions;
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
